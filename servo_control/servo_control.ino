f79b2c0f60a280178d61c870a89563b832c2429d
Sidharth Venkatesh <sidv@purdue.edu>
2018-07-07 00:00:19 -0700

    Testing motion with LED

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -2,6 +2,10 @@
  
 Servo myservo;
 
+int ledPin = 6;
+int motionPin = 4;
+int motionVal = 0;
+int pirState = LOW;
 int powerPin = 7;
 int servoPin = 9;
 String authorized = "";
@@ -10,21 +14,44 @@ void setup()
 { 
   Serial.begin(9600);
   pinMode(powerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(motionPin, INPUT);
   myservo.attach(servoPin);
 } 
  
  
 void loop() 
 {
-  if (Serial.available()) {
-    authorized = Serial.readString();
-    if (authorized == "on") {
-      digitalWrite(powerPin, LOW);
-      Serial.println("Authorized");
-    } else if (authorized == "off") {
-      digitalWrite(powerPin, HIGH);
-      Serial.println("Not authorized");
-    }
+  motionVal = digitalRead(motionPin);
+  if(Serial.available())
+  {
+  
+  	if(motionVal == HIGH)
+  	{
+		digitalWrite(ledPin, HIGH);
+		if(pirState == LOW)
+		{
+			Serial.println("Motion detected\n");
+			pirState = HIGH;
+		}
+  	}
+  	else
+  	{
+		digitalWrite(ledPin, LOW);
+		if(pirState == HIGH)
+		{
+			Serial.println("Motion stopped\n");
+			pirState = LOW;
+		}
+  	}	
+        authorized = Serial.readString();
+	if (authorized == "on") {
+        digitalWrite(powerPin, LOW);
+        Serial.println("Authorized\n");
+        } else if (authorized == "off") {
+        digitalWrite(powerPin, HIGH);
+        Serial.println("Not authorized\n");
+        }  
   }
   myservo.write(0);
   delay(15);
