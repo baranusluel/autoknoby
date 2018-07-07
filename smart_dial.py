d54edd0892c14ca62daca8614027bd6d41e22e69
Advaith <advaiths@gmail.com>
2018-07-06 23:31:13 -0700

    added initial python script

---
+++ smart_dial.py
@@ -0,0 +1,10 @@
+import string
+import cognitive_face as CF
+
+KEY = '96ac51d9ccf74f258ecfc1ae8ece5e44'
+CF.Key.set(KEY)
+
+BASE_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0'
+CF.BaseUrl.set(BASE_URL)
+
+
