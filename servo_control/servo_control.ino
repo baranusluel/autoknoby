c071035523af9e0173ad6f554a63d20746440e05
Sidharth Venkatesh <sidv@purdue.edu>
2018-07-07 00:09:29 -0700

    Fixed serial problem

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -23,36 +23,37 @@ void setup()
 void loop() 
 {
   motionVal = digitalRead(motionPin);
+ 
+  if(motionVal == HIGH)
+  {
+	digitalWrite(ledPin, HIGH);
+	if(pirState == LOW)
+	{
+		Serial.println("Motion detected\n");
+		pirState = HIGH;
+	}
+  }
+  else
+  {
+	digitalWrite(ledPin, LOW);
+	if(pirState == HIGH)
+	{
+		Serial.println("Motion stopped\n");
+		pirState = LOW;
+	}
+  }
   if(Serial.available())
   {
-  
-  	if(motionVal == HIGH)
-  	{
-		digitalWrite(ledPin, HIGH);
-		if(pirState == LOW)
-		{
-			Serial.println("Motion detected\n");
-			pirState = HIGH;
-		}
-  	}
-  	else
-  	{
-		digitalWrite(ledPin, LOW);
-		if(pirState == HIGH)
-		{
-			Serial.println("Motion stopped\n");
-			pirState = LOW;
-		}
-  	}	
         authorized = Serial.readString();
 	if (authorized == "on") {
-        digitalWrite(powerPin, LOW);
-        Serial.println("Authorized\n");
+        	digitalWrite(powerPin, LOW);
+	        Serial.println("Authorized\n");
         } else if (authorized == "off") {
-        digitalWrite(powerPin, HIGH);
-        Serial.println("Not authorized\n");
+        	digitalWrite(powerPin, HIGH);
+	        Serial.println("Not authorized\n");
         }  
   }
   myservo.write(0);
   delay(15);
-} 
+}
+
